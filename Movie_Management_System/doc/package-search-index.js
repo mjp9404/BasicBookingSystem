06a3bf48c23de6648cd3a8df4e0dd4a9cc3637af
Min <99421557+mjp9404@users.noreply.github.com>
2023-01-08 21:10:59 -0700

    Delete Movie_Management_System/doc directory

--- Movie_Management_System/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"sait.mms.application"},{"l":"sait.mms.contracts"},{"l":"sait.mms.drivers"},{"l":"sait.mms.managers"},{"l":"sait.mms.problemdomain"}]
--- Movie_Management_System/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"sait.mms.application","l":"appDriver"},{"p":"sait.mms.contracts","l":"DatabaseDriver"},{"p":"sait.mms.drivers","l":"MariaDBDriver"},{"p":"sait.mms.problemdomain","l":"Movie"},{"p":"sait.mms.managers","l":"MovieManagementSystem"}]
